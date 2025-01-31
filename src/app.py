66e93803bf518c2e995c55de7e7c63e750b17d0b
JZhg02 <94388221+JZhg02@users.noreply.github.com>
2025-01-31 14:50:38 +0100

    embed documents app

--- src/app.py
+++ src/app.py
@@ -59,15 +59,15 @@ def initialize_rag():
     if not os.path.isdir(extracted_text_folder):
         pdf_to_text.extract_text_from_each_document(documents_folder=documents_folder, extracted_text_folder=extracted_text_folder)
 
-    # Create FAISS index if not already done
-    if not os.path.isdir(index_folder):
-        # Create FAISS index and add embeddings
-        embed_documents.create_faiss_index_and_embeddings_if_not_exists(tokenizer=tokenizer, embeddings_model=model, index_folder=index_folder, extracted_text_folder=extracted_text_folder, dimension=dimension)
-        faiss_index_file = os.path.join(index_folder, "faiss_index_with_metadata.bin")
-        metadata_file = os.path.join(index_folder, "metadata.json")
-    else: 
-        faiss_index_file = os.path.join(index_folder, "faiss_index_with_metadata.bin")
-        metadata_file = os.path.join(index_folder, "metadata.json")
+    # # Create FAISS index if not already done
+    # if not os.path.isdir(index_folder):
+    #     # Create FAISS index and add embeddings
+    embed_documents.create_faiss_index_and_embeddings_if_not_exists(tokenizer=tokenizer, embeddings_model=model, index_folder=index_folder, extracted_text_folder=extracted_text_folder, dimension=dimension)
+    faiss_index_file = os.path.join(index_folder, "faiss_index_with_metadata.bin")
+    metadata_file = os.path.join(index_folder, "metadata.json")
+    # else: 
+    #     faiss_index_file = os.path.join(index_folder, "faiss_index_with_metadata.bin")
+    #     metadata_file = os.path.join(index_folder, "metadata.json")
     
     # Load FAISS index
     if not os.path.exists(faiss_index_file):
